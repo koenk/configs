14b7fb043c1533dbdd106ef22d320645a73d2fac
Koen Koning <koenkoning@gmail.com>
2013-04-21 15:03:17 +0200

    Fix awesome crash when sound device not found

--- awesome_rc.lua
+++ awesome_rc.lua
@@ -177,12 +177,16 @@ volumecfg.mixercommand = function (command)
     fd:close()
 
     local volume = string.match(status, "(%d?%d?%d)%%")
-    volume = string.format("% 3d", volume)
-    status = string.match(status, "%[(o[^%]]*)%]")
-    if string.find(status, "on", 1, true) then
-        volume = volume .. "%"
+    if not volume then
+        volume = "EE"
     else
-        volume = volume .. "M"
+        volume = string.format("% 3d", volume)
+        status = string.match(status, "%[(o[^%]]*)%]")
+        if string.find(status, "on", 1, true) then
+            volume = volume .. "%"
+        else
+            volume = volume .. "M"
+        end
     end
     volumecfg.widget:set_text(volume)
 end
